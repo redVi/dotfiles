81af82fd8d2539519c542ed4a1f5219cbd290850
redVi <liberty195@gmail.com>
2013-02-25 16:56:27 +0000

    delete some files

--- unzip.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('pclzip.lib.php');
-$archive = new PclZip('archive.zip');
-if ($archive->extract() == 0) {
-    die("Error : ".$archive->errorInfo(true));
-}else{
-    echo 'ok';
-}
-?>

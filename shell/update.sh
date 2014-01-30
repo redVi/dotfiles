c4245693212fd2c9b300a732be03a8fa628942ae
redVi <liberty195@gmail.com>
2014-01-30 19:15:03 +0700

    add conky small and update openbox config files

--- shell/update.sh
+++ shell/update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+# --- gentoo --- #
 # sudo emerge-webrsync && sudo emerge -auvDN world
+
+# -- debian -- #
+
 sudo aptitude update
 printf "update system?[y/n] "
 read answer
 if [ "$answer" != "y" ]; then
   echo "exit"
-else 
+else
   sudo aptitude upgrade
 fi
 
